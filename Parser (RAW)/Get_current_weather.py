5f15727faf902b7f96eb4c28a10174869615c94e
NaSheverdov <nikfilk2030@gmail.com>
2021-10-23 14:06:08 +0300

    changed all keys system

--- Parser (RAW)/Get_current_weather.py
+++ Parser (RAW)/Get_current_weather.py
@@ -1,8 +1,9 @@
 from pyowm.owm import OWM
 import pandas as pd
 from datetime import datetime
+import keys
 
-owm = OWM('e05eba28a0a4ca402ea17fed7b7a208f')
+owm = OWM(keys.owm_token)
 reg = owm.city_id_registry()
 mgr = owm.weather_manager()
 
@@ -11,6 +12,16 @@ def get_current_weather(lat, lng):
     observation = mgr.weather_at_coords(lat, lng)
     w = observation.weather
     now = datetime.now()
+
+    print(f'humidity {w.humidity}\n'
+          f'pressure: {w.pressure["press"]}\n'
+          f'tempC: {w.temperature("celsius")["temp"]}\n'
+          f'windspeedKmph: {w.wind()["speed"]}\n'
+          f'year: {now.year}\n'
+          f'day: {now.day}\n'
+          f'hour: {now.hour}\n'
+          f'lat: {lat}\n'
+          f'lng: {lng}')
 
     data = {'humidity': [w.humidity],
             'pressure': [w.pressure["press"]],

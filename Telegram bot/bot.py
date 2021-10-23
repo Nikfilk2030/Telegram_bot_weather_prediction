b26c49b664f46d099be9d1051f368bedf1e69fa0
NaSheverdov <nikfilk2030@gmail.com>
2021-10-23 22:22:45 +0300

    Added new function to bot, like rebooting. changed a meme, and added instruction to location

--- Telegram bot/bot.py
+++ Telegram bot/bot.py
@@ -3,51 +3,67 @@ from telebot import types
 import keys
 import pandas as pd
 
+# taken bot token from keys.py
 token = keys.bot_token
 
+# init bot
 bot = telebot.TeleBot(token)
 
 # buttons description
 button_text1 = 'Predict weather'
-button_text2 = 'Get the Gosling meme'
+button_text2 = 'Get a dog meme'
+button_text3 = 'Reboot bot'
 
 # dictionary with simple command -> answer description
-messages_and_answers = {button_text1: "Send your geolocation:",
-                        "Tell me a joke": "When I see lovers' names carved in"
+messages_and_answers = {"Tell me a joke": "When I see lovers' names carved in"
                                           " a tree, I don't think it's sweet. I"
                                           " just think it's surprising how many"
-                                          " people bring a knife on a date."}
+                                          " people bring a knife on a date.",
+                        "Hello": "Hello",
+                        "What can you do?": "How much are you ready to pay?"}
 
 
 @bot.message_handler(commands=['start'])
-def button_message(message):
+def button_maker(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton(button_text1)
-    item2 = types.KeyboardButton(button_text2)
-    markup.add(item1, item2)
-
-    bot.send_message(message.chat.id, 'Choose a button:', reply_markup=markup)
+    button1 = types.KeyboardButton(button_text1)
+    button2 = types.KeyboardButton(button_text2)
+    button3 = types.KeyboardButton(button_text3)
+    markup.add(button3, button2)
+    markup.add(button1)
+    # Starting words:
+    bot.send_message(message.chat.id, 'Hello! Choose a button', reply_markup=markup)
 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text in messages_and_answers:
+    if message.text in messages_and_answers:  # answering on simple things
         bot.send_message(message.chat.id, messages_and_answers[message.text])
+
+    elif message.text == button_text1:
+        bot.send_message(message.chat.id, "Send your geolocation, you have to use this button:")
+        geoposition_instruction_photo = open('geoposition.jpg', 'rb')
+        bot.send_photo(message.chat.id, geoposition_instruction_photo)
+
     elif message.text == button_text2:
-        photo = open('Gosling.jpg', 'rb')
-        bot.send_message(message.chat.id, 'Sending a photo:')
-        bot.send_photo(message.chat.id, photo)
+        dog_meme_photo = open('dog_meme.jpg', 'rb')
+        bot.send_message(message.chat.id, 'Sending a photo...')
+        bot.send_photo(message.chat.id, dog_meme_photo)
+
+    elif message.text == button_text3:
+        button_maker(message)
+
     else:
         bot.send_message(message.chat.id, "Sorry, i can't understand this command yet")
 
 
 @bot.message_handler(content_types='location')
 def geolocation_reply(message):
-    lng = message.location.longitude
-    lat = message.location.latitude
+    lng, lat = message.location.longitude, message.location.latitude
 
-    bot.send_message(message.chat.id, f'Your location: longitude {message.location.longitude},'
-                                      f' latitude {message.location.latitude}')
+    bot.send_message(message.chat.id, f'Your location:\n'
+                                      f'longitude {lng}\n'
+                                      f'latitude {lat}')
 
 
 bot.infinity_polling()
